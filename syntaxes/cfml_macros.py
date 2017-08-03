bda61925696219bd388f5e69e52a94674d512792
jcberquist <jcberquist@outlook.com>
2017-08-03 13:51:10 -0700

    Made attribute macro more robust

--- syntaxes/cfml_macros.py
+++ syntaxes/cfml_macros.py
@@ -1,65 +1,29 @@
-from YAMLMacros.lib.syntax import meta, expect, pop_on, stack
-
-def attribute_string(char, scope, value_scope):
-    return {
-        'match': char,
-        'scope': 'punctuation.definition.string.begin.cfml',
-        'set': [
-            {
-                'meta_scope': 'meta.string.quoted.{scope}.cfml string.quoted.{scope}.cfml'.format(scope = scope)
-            },
-            {
-                'meta_content_scope': value_scope
-            },
-            {
-                'match': char * 2,
-                'scope': 'constant.character.escape.quote.cfml',
-            },
-            {
-                'match': char,
-                'scope': 'punctuation.definition.string.end.cfml',
-                'pop': True
-            }
-        ]
-    }
+from CFML.syntaxes import cfml_syntax
 
 def attribute(name, value_scope, name_scope=None, meta_scope=None):
     syntax = {
-        'match': r'(?i:\b%s\b)' % name,
+        'match': r'(?i:\b(?:%s)\b)' % name,
         'scope': 'entity.other.attribute-name.cfml' + (' ' + name_scope if name_scope else ''),
         'push': [
             {
                 'match': '=',
                 'scope': 'punctuation.separator.key-value.cfml',
                 'set': [
-                    attribute_string("'", "single", value_scope),
-                    attribute_string('"', "double", value_scope),
+                    cfml_syntax.attribute_value_string("'", "single", value_scope),
+                    cfml_syntax.attribute_value_string('"', "double", value_scope),
+                    cfml_syntax.attribute_value_unquoted(value_scope),
                     {
-                        'match': '(?=[^\s</>{;])',
-                        'set': [
-                            {
-                                'meta_scope': 'meta.string.unquoted.cfml string.unquoted.cfml ' + value_scope
-                            },
-                            {
-                                'match': '(?=[\s</>{;])',
-                                'pop': True
-                            }
-                        ]
-                    },
-                    {
-                        'match': '(?=\S)',
-                        'pop': True
+                        'include': 'else-pop'
                     }
                 ]
             },
             {
-                'match': '(?=\S)',
-                'pop': True
+                'include': 'else-pop'
             }
         ]
     }
 
     if meta_scope:
-        syntax['push'] = [meta(meta_scope), syntax['push']]
+        syntax['push'] = [cfml_syntax.meta(meta_scope), syntax['push']]
 
-    return syntax
+    return cfml_syntax.order_output(syntax)
---
+++ syntaxes/cfml_syntax.py
@@ -0,0 +1,88 @@
+from ruamel.yaml.comments import CommentedMap
+from YAMLMacros.lib.syntax import meta, expect, pop_on, stack
+
+ORDERED_KEYS = ['meta_scope', 'meta_content_scope', 'match', 'scope', 'captures', 'push', 'set', 'pop']
+
+def order_output(syntax):
+    if isinstance(syntax, dict):
+        ordered_syntax = CommentedMap()
+        for key in ORDERED_KEYS:
+            if key in syntax:
+                ordered_syntax[key] = order_output(syntax[key])
+        for key in syntax:
+            if key not in ORDERED_KEYS:
+                ordered_syntax[key] = order_output(syntax[key])
+        return ordered_syntax
+    if isinstance(syntax, list):
+        return [order_output(item) for item in syntax]
+    return syntax
+
+def attribute_value_string(char, scope, value_scope):
+    syntax = {
+        'match': char,
+        'scope': 'punctuation.definition.string.begin.cfml',
+        'set': [
+            [
+                {
+                    'meta_scope': 'meta.string.quoted.{scope}.cfml string.quoted.{scope}.cfml'.format(scope = scope)
+                },
+                {
+                    'match': char,
+                    'scope': 'punctuation.definition.string.end.cfml',
+                    'pop': True
+                }
+            ]
+        ]
+    }
+
+    if '.' in value_scope and 'scope:' not in value_scope:
+        # we have a scope name
+        syntax['set'].append([
+            {
+                'meta_scope': value_scope
+            },
+            {
+                'match': char * 2,
+                'scope': 'constant.character.escape.quote.cfml',
+            },
+            {
+                'match': r'(?=%s)' % char,
+                'pop': True
+            }
+        ])
+    else:
+        # this is a context
+        syntax['set'].append([
+            {'include': value_scope},
+            {'include': 'else-pop'}
+        ])
+
+    return syntax
+
+def attribute_value_unquoted(value_scope):
+    syntax = {
+        'match': r'(?=[A-Za-z0-9\-_.$])',
+        'set': [
+            meta('meta.string.unquoted.cfml string.unquoted.cfml')
+        ]
+    }
+
+    if '.' in value_scope and 'scope:' not in value_scope:
+        # we have a scope name
+        syntax['set'].append([
+            {
+                'meta_scope': value_scope
+            },
+            {
+                'match': r'(?=[^A-Za-z0-9\-_.$])',
+                'pop': True
+            }
+        ])
+    else:
+        # this is a context
+        syntax['set'].append([
+            {'include': value_scope},
+            {'include': 'else-pop'}
+        ])
+
+    return syntax

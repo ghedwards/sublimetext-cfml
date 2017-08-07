f60ebdbd084ce655ae141aa458dab225651923ec
jcberquist <jcberquist@outlook.com>
2017-08-07 08:22:26 -0700

    Started using `.yaml-macros` for CFScript syntax

--- syntaxes/cfml_macros.py
+++ syntaxes/cfml_macros.py
@@ -27,3 +27,38 @@ def attribute(name, value_scope, name_scope=None, meta_scope=None):
         syntax['push'] = [cfml_syntax.meta(meta_scope), syntax['push']]
 
     return cfml_syntax.order_output(syntax)
+
+
+def function_call_params(meta_scope, named_param_scope, delimiter_scope):
+    syntax = {
+        'match': r'\(',
+        'scope': 'punctuation.section.group.begin.cfml',
+        'set': [
+            {
+                'meta_scope': meta_scope
+            },
+            {
+                'match': r'\)',
+                'scope': 'punctuation.section.group.end.cfml',
+                'pop': True
+            },
+            {
+                'match': ',',
+                'scope': delimiter_scope
+            },
+            {
+                'match': r'\b({{identifier}})\s*(=)(?!=)',
+                'captures': {
+                    '1': named_param_scope,
+                    '2': 'keyword.operator.assignment.cfml'
+                },
+                'push': 'expression-no-comma'
+            },
+            {
+                'match': r'(?=\S)',
+                'push': 'expression-no-comma'
+            }
+        ]
+    }
+
+    return cfml_syntax.order_output(syntax)
